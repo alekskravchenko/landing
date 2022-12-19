a0b048ec8c702c7c5ecc7fd1d1f36134bb0e2f34
Aleks Kravchenko <web.kravchenko@gmail.com>
2022-12-19 13:07:36 +0200

    add form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import axios from "axios";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const config = {
+  headers:{
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const products = [
+    [ 
+      {
+        'product_id': '4',
+        'price': '3299',
+        'count': '2',
+      }
+    ]
+]
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: { key: 'ea76d526ee2d548f956e38845476d6fd', country: 'UA', delivery: 1, products: products},
+    
+    onSubmit: values => {
+      const orderId = Date.now;
+      //key fa66b7ff72bdf1ca06a0699d1d8863e5
+      //order_id
+      //country UA
+      //products
+            // 'product_id' => '5',    //код товара (из каталога CRM)
+            // 'price'      => '3299', //цена товара 1
+            // 'count'      => '1'
+      //bayer_name
+      //phone
+      //delivery 1
+      //delivery_adress
+
+      //alert(JSON.stringify(values, null, 2));
+      
+
+      var config = {
+        method: 'get',
+        url: `https://cors-anywhere.herokuapp.com/http://test2.lp-crm.biz/api/addNewOrder.html?key=${values.key}&order_id=${orderId}&country=UA&bayer_name=${values.bayer_name}&phone=${values.phone}&site=localhost&delivery=${values.delivery}&delivery_adress=${values.delivery_adress}&products=${values.products}`,
+        data : ''
+      };
+      
+      axios(config)
+        .then(function (res) {
+           console.log(res.data)
+           alert('Successfully signed up!');
+        })
+        .catch(function (res) {
+           console.log(res)
+      });
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="bayer_name">Ім'я</label>
+      <input
+        id="bayer_name"
+        name="bayer_name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.bayer_name}
+      />
+      <label htmlFor="phone">Телефон</label>
+      <input
+        id="phone"
+        name="phone"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.phone}
+      />
+      <label htmlFor="delivery_adress">Відділення Нової Пошти</label>
+      <input
+        id="delivery_adress"
+        name="delivery_adress"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.delivery_adress}
+      />
+      <button type="submit">Купити</button>
+    </form>
+  );
+};
+
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
